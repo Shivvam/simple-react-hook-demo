8bf631841d669a3904cfaa68f9f5c79502f2c3f4
Shivam <svishwakarma74@gmail.com>
2019-08-18 19:24:00 +0530

    DRY principle

--- src/components/table.js
+++ src/components/table.js
@@ -2,10 +2,11 @@ import React, {useState} from 'react';
 import Row from './row';
 const Table = () => {
   const [rowCount,setCount] = useState(1);
+  const [maxRows,minRows] = [10,1];
   return(
     <div>
-    <button onClick={() => setCount(rowCount+1 > 10 ? 10 : rowCount+1 )  } disabled={rowCount >= 10 ? true : false}>Add Row</button>  &nbsp;&nbsp;
-    <button onClick={() => setCount(rowCount-1 > 0 ? rowCount-1 : 1 )  }  disabled={rowCount <= 1 ? true : false} >Delete Row</button>
+    <button onClick={() => setCount(rowCount+1 > maxRows ? maxRows : rowCount+1 )  } disabled={rowCount >= maxRows ? true : false}>Add Row</button>  &nbsp;&nbsp;
+    <button onClick={() => setCount(rowCount-1 >= minRows ? rowCount-1 : minRows )  }  disabled={rowCount <= minRows ? true : false} >Delete Row</button>
       <hr/>
       Total Number of Rows : {rowCount}
       <hr/>
